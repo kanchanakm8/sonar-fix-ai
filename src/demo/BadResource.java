4eeea9c5959d0bfe42b917d076d819424eec81c8
kanchanakm8 <kanchana.kambalimath@gmail.com>
2025-12-11 16:17:01 +0000

    revert

--- src/demo/BadResource.java
+++ src/demo/BadResource.java
@@ -5,13 +5,14 @@ import java.io.IOException;
 
 public class BadResource {
     public static void readFile(String path) throws IOException {
-        try (FileInputStream fis = new FileInputStream(path)) {
-            int data = fis.read();
-            while (data != -1) {
-                System.out.print((char) data);
-                data = fis.read();
-            }
+        FileInputStream fis = new FileInputStream(path);
+        // Sonar issue: resource opened but not closed -> resource leak
+        int data = fis.read();
+        while (data != -1) {
+            System.out.print((char) data);
+            data = fis.read();
         }
+        // missing fis.close();
     }
 
     public static void main(String[] args) throws IOException {
